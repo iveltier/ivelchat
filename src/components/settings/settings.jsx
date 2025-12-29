bf1ce21f7239ee42e22c9afe66c103fb876b6e41
iveltier <iveltier@mailbox.org>
2025-12-29 02:52:58 +0100

    profilbilder added, styled profil pictures window

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -17,12 +17,60 @@ function Settings() {
 		return () => clearInterval(interval);
 	}, []);
 
+	function handleProfilePictureSrc(name) {
+		return `/images/profilePictures/user/${name}`;
+	}
+
 	return (
 		<div className={styles.settingsContainer}>
 			<Logo />
 
 			<h1>Settings</h1>
-			<div className={styles.changeProfilePictureWindow}></div>
+			<div className={styles.changeProfilePictureWindow}>
+				<h2>Change your profile picture</h2>
+				<div className={styles.profilePicturesWrapper}>
+					<img
+						src={handleProfilePictureSrc("cookiemonster.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("elmo.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("peter.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("killua.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("patrick.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("sandmann.jpeg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("ernie.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("kadse.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("cute.jpg")}
+						className={styles.profilePictures}
+					/>
+					<img
+						src={handleProfilePictureSrc("default.jpg")}
+						className={styles.profilePictures}
+					/>
+				</div>
+			</div>
 			<div className={styles.settingsWrapper}>
 				<div>
 					<img src={currentUserPicture} className={styles.currentUserPicture} />
@@ -42,22 +90,24 @@ function Settings() {
 				</div>
 
 				<table className={styles.settingsTable}>
-					<tr>
-						<td>Color Scheme</td>
-						<td></td>
-					</tr>
-					<tr>
-						<td>Timestamps</td>
-						<td></td>
-					</tr>
-					<tr>
-						<td>Monospace</td>
-						<td></td>
-					</tr>
-					<tr>
-						<td>24h Format</td>
-						<td></td>
-					</tr>
+					<tbody>
+						<tr>
+							<td>Color Scheme</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>Timestamps</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>Monospace</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>24h Format</td>
+							<td></td>
+						</tr>
+					</tbody>
 				</table>
 			</div>
 		</div>
