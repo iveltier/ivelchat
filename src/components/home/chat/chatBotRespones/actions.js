c63019965dc40b1730868e70fd6eb85df235df4c
iveltier <iveltier@mailbox.org>
2026-01-05 22:14:18 +0100

    dice action

--- src/components/home/chat/chatBotRespones/actions.js
+++ src/components/home/chat/chatBotRespones/actions.js
@@ -4,4 +4,7 @@ export const actions = {
 	flipCoin() {
 		return Math.random() < 0.5 ? "You got heads!" : "You got tails!";
 	},
+	dice() {
+		return `You got a ${Math.floor(Math.random() * 6 + 1)}`;
+	},
 };
